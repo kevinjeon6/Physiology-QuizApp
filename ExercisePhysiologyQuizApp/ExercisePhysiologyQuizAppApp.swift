8659d011ce402665c9866d28b5f5767314755b0a
Kevin Mattocks <kevin.t.mattocks@gmail.com>
2022-04-22 15:37:37 -0500

    Initial Commit

---
+++ ExercisePhysiologyQuizApp/ExercisePhysiologyQuizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExercisePhysiologyQuizAppApp.swift
+//  ExercisePhysiologyQuizApp
+//
+//  Created by Kevin Mattocks on 4/22/22.
+//
+
+import SwiftUI
+
+@main
+struct ExercisePhysiologyQuizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
