96e0064fcfefe998ebe0c1b54173820c661cc590
Kevin Mattocks <kevin.t.mattocks@gmail.com>
2022-04-25 15:06:40 -0500

    Insert NavigationView with title Add Quiz model to represent data

---
+++ ExercisePhysiologyQuizApp/Model/Quiz.swift
@@ -0,0 +1,37 @@
+//
+//  Quiz.swift
+//  ExercisePhysiologyQuizApp
+//
+//  Created by Kevin Mattocks on 4/25/22.
+//
+
+import Foundation
+
+
+struct Quiz: Decodable, Identifiable {
+    
+    var id: Int
+    var category: String
+    var content: Content
+}
+
+
+struct Content: Decodable, Identifiable {
+    var id: Int
+    var image: String
+    var description: String
+    var test: Test
+}
+
+
+struct Test: Decodable, Identifiable {
+    var id: Int
+    var questions: [Questions]
+}
+
+struct Questions: Decodable, Identifiable {
+    var id: Int
+    var content: String
+    var correctIndex: Int
+    var answers: [String]
+}
--- ExercisePhysiologyQuizApp/Views/ContentView.swift
+++ ExercisePhysiologyQuizApp/Views/ContentView.swift
@@ -9,8 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            VStack{
+                Text("Main screen of different quiz options")
+            }
+            .navigationTitle("Quizzes")
+        }
     }
 }
 
