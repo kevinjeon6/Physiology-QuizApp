19670512719c0f5c1b125d578e2a9f29a2863468
Kevin Mattocks <kevin.t.mattocks@gmail.com>
2022-04-26 14:55:41 -0500

    Create reusable CourseCard for title, description, and image of the different quizzes

---
+++ ExercisePhysiologyQuizApp/Components/CourseCard.swift
@@ -0,0 +1,20 @@
+//
+//  CourseCard.swift
+//  ExercisePhysiologyQuizApp
+//
+//  Created by Kevin Mattocks on 4/26/22.
+//
+
+import SwiftUI
+
+struct CourseCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CourseCard_Previews: PreviewProvider {
+    static var previews: some View {
+        CourseCard()
+    }
+}
