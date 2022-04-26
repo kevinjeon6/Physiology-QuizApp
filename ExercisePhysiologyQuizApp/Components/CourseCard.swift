e1e75cf4926c725f8151f1277d90f8a84a546031
Kevin Mattocks <kevin.t.mattocks@gmail.com>
2022-04-26 15:18:48 -0500

    Implement reusable CourseCard to display quiz course information

--- ExercisePhysiologyQuizApp/Components/CourseCard.swift
+++ ExercisePhysiologyQuizApp/Components/CourseCard.swift
@@ -8,13 +8,48 @@
 import SwiftUI
 
 struct CourseCard: View {
+    
+    var category: String
+    var description: String
+    var image: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        
+        ZStack(alignment: .leading){
+            RoundedRectangle(cornerRadius: 20)
+                .fill(Color.indigo)
+                .aspectRatio(CGSize(width: 200, height: 250), contentMode: .fit)
+                .shadow(radius: 10)
+            
+            VStack(alignment: .leading, spacing: 10) {
+                Text(category)
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .fontWeight(.bold)
+              
+                
+                Text(description)
+                    .font(.headline)
+               Spacer()
+                
+                Image(image)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(minWidth: 200, maxWidth: .infinity, minHeight: 200, maxHeight: 300)
+                    Spacer()
+                
+            }//Vstack
+            .padding()
+
+        }//Zstack
+        
+        
     }
 }
 
 struct CourseCard_Previews: PreviewProvider {
     static var previews: some View {
-        CourseCard()
+        CourseCard(category: "Skeletal Muscle", description: "Muscle", image: "muscle")
     }
 }
--- ExercisePhysiologyQuizApp/Views/ContentView.swift
+++ ExercisePhysiologyQuizApp/Views/ContentView.swift
@@ -19,35 +19,9 @@ struct ContentView: View {
                 ForEach(model.quizModules) {
                     quiz in
                     
-                    ZStack(alignment: .leading){
-                        RoundedRectangle(cornerRadius: 20)
-                            .fill(Color.indigo)
-                            .aspectRatio(CGSize(width: 200, height: 250), contentMode: .fit)
-                            .shadow(radius: 10)
-                        
-                        VStack(alignment: .leading, spacing: 10) {
-                            Text(quiz.category)
-                                .font(.title)
-                                .foregroundColor(.white)
-                                .fontWeight(.bold)
-                          
-                            
-                            Text(quiz.course.description)
-                                .font(.headline)
-                           Spacer()
-                            
-                            Image("\(quiz.course.image)")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(minWidth: 200, maxWidth: .infinity, minHeight: 200, maxHeight: 300)
-                                Spacer()
-                            
-                        }//Vstack
-                        .padding()
-                      
-                        
-                        
-                    }
+                    CourseCard(category: quiz.category, description: quiz.course.description, image: quiz.course.image)
+                    
+                
                 }//ForEach Loop
                
                
