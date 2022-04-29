49606bbbe89c033a7a9ded45e1820e0a40fa1ca1
Kevin Mattocks <kevin.t.mattocks@gmail.com>
2022-04-29 14:27:47 -0500

    Add property to keep track of how many answers are correct Update capsule button for submission look Add disable modifier to answer and submission button

--- ExercisePhysiologyQuizApp/Views/QuizView.swift
+++ ExercisePhysiologyQuizApp/Views/QuizView.swift
@@ -12,19 +12,23 @@ struct QuizView: View {
     @EnvironmentObject var model: ContentViewModel
     
     @State private var selectedAnswer: Int?
+    @State private var numberCorrect = 0
+    @State private var isSubmitted = false
     
     var body: some View {
-      
+        
         if model.currentQuestion != nil {
             
             VStack(alignment: .leading) {
                 Text("\(model.currentQuestionIndex + 1) of \(model.currentQuiz?.course.test.questions.count ?? 0)")
-                    
-                    
+                    .padding(.bottom, 10)
+                
+                
                 
                 Text("\(model.currentQuestion?.content ?? "")")
-                    
                 
+               
+                //Answers
                 ScrollView {
                     VStack {
                         ForEach(0..<model.currentQuestion!.answers.count, id: \.self){
@@ -41,31 +45,60 @@ struct QuizView: View {
                                     
                                     Text("\(model.currentQuestion?.answers[index] ?? "")")
                                         .padding()
+                                        .foregroundColor(.black)
                                 }
                             }
-                            .accentColor(.black)
+                            .disabled(isSubmitted)
+                           
                             
-
+                            
                         }//ForEach
                     }//Vstack
                 }//ScrollView
-
+                
+                
+                
+                
+                //Submit/Next/Finish Button
+                Button {
+                    isSubmitted.toggle()
+                    
+                    if selectedAnswer == model.currentQuestion!.correctIndex {
+                        numberCorrect += 1
+                    }
+                } label: {
+                    ZStack {
+                        Capsule()
+                            .strokeBorder(Color.orange, lineWidth: 4)
+                            .background(Capsule().fill(Color.blue))
+                            .frame(height: 50)
+                     
+                        
+                        Text("Submit")
+                            .padding()
+                            .foregroundColor(.white)
+                    }
+                }
+                .disabled(selectedAnswer == nil)
+              
+                
+                
             }//Vstack
             .navigationBarTitle("\(model.currentQuiz?.category ?? "")")
             .padding(.horizontal, 20)
         } else {
             ProgressView()
         }
-   
+        
         
         
     }//body
 }
 
-struct QuizView_Previews: PreviewProvider {
-    static var previews: some View {
-        QuizView()
-            .environmentObject(ContentViewModel())
-           
-    }
-}
+//struct QuizView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        QuizView()
+//            .environmentObject(ContentViewModel())
+//
+//    }
+//}
