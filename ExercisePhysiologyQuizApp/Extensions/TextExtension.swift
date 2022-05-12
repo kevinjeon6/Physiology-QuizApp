fe330d6679563682924c0974bd67852b059a96df
Kevin Mattocks <kevin.t.mattocks@gmail.com>
2022-05-12 12:59:00 -0500

    Create Text extension to abstract code for the button style text for the answers and submit button

---
+++ ExercisePhysiologyQuizApp/Extensions/TextExtension.swift
@@ -0,0 +1,19 @@
+//
+//  SubmitExtension.swift
+//  ExercisePhysiologyQuizApp
+//
+//  Created by Kevin Mattocks on 5/6/22.
+//
+
+import SwiftUI
+
+
+extension Text {
+    func ButtonTextStyle() -> some View {
+        self
+            .font(.body)
+            .fontWeight(.bold)
+            .padding()
+            .foregroundColor(.white)
+    }
+}
