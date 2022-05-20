515ec4dcf6430b9b073ec765ae90846f7d804a7c
Kevin Mattocks <kevin.t.mattocks@gmail.com>
2022-05-20 14:23:24 -0500

    Add color scheme Create color extension file for color scheme

---
+++ ExercisePhysiologyQuizApp/Colors/ColorExtension.swift
@@ -0,0 +1,16 @@
+//
+//  ColorScheme.swift
+//  ExercisePhysiologyQuizApp
+//
+//  Created by Kevin Mattocks on 5/20/22.
+//
+
+import SwiftUI
+
+
+extension Color {
+    static let backgroundColor = Color("background-color")
+    static let cardColor = Color("card-color")
+    static let buttonColor = Color("button-color")
+    static let tertiaryColor = Color("tertiary-color")
+}
